393c35cf6df37fed1ef4b1b1312e01cfe53053fb
Troels Korreman <troels.korreman@gmail.com>
2023-12-06 12:45:45 +0100

    day06: Initial optimized solution

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ fn main() {
         state.run_day(3, &solutions::day03::run);
         state.run_day(4, &solutions::day04::run);
         state.run_day(5, &solutions::day05::run);
-        state.run_day(6, &solutions::day06::run);
+        state.run_day(6, &optimized::day06::run);
         //state.run_day(7, &solutions::day07::run);
         //state.run_day(8, &solutions::day08::run);
         //state.run_day(9, &solutions::day09::run);
---
+++ src/optimized/day06.rs
@@ -0,0 +1,33 @@
+use crate::util::parsers::Numbers;
+use itertools::Itertools;
+
+pub fn run(input: &str) -> (u64, u64) {
+    // Parse
+    let (times, dists) = input.split_once('\n').unwrap();
+    let times = Numbers::<u64>::new(times);
+    let dists = Numbers::<u64>::new(dists);
+
+    // Part 1
+    let mut part1 = 1;
+    for (time, dist) in times.zip(dists) {
+        part1 *= ways_to_beat(time, dist);
+    }
+
+    // Part 2
+    let input: String = input
+        .chars()
+        .filter(|x| x.is_ascii_digit() || *x == '\n')
+        .collect();
+    let (time, dist): (u64, u64) = Numbers::new(&input).collect_tuple().unwrap();
+    let part2 = ways_to_beat(time, dist);
+
+    (part1, part2)
+}
+
+#[inline(always)]
+fn ways_to_beat(time: u64, dist: u64) -> u64 {
+    let (t, d) = (time as f64, dist as f64);
+    let f = (t - (t.powi(2) - 4.0 * d).sqrt()) / 2.0;
+    let f = f.ceil() as u64;
+    time - f * 2 + 1
+}
