2e2ca84b092c12d1964e99ad9490c824c861c347
Troels Korreman <troels.korreman@gmail.com>
2023-12-06 08:25:04 +0100

    day06: Clean up solution

--- src/solutions/day06.rs
+++ src/solutions/day06.rs
@@ -2,47 +2,39 @@ use itertools::Itertools;
 
 use crate::util::parsers::Numbers;
 
-pub fn run(input: &str) -> (u32, u32) {
+pub fn run(input: &str) -> (u64, u64) {
     // Parse
-    let (times, dists) = input
-        .split_once('\n')
-        .unwrap();
-    let times: Vec<u32> = Numbers::new(times).collect();
-    let dists: Vec<u32> = Numbers::new(dists).collect();
+    let (times, dists) = input.split_once('\n').unwrap();
+    let times: Vec<u64> = Numbers::new(times).collect();
+    let dists: Vec<u64> = Numbers::new(dists).collect();
 
     // Part 1
     let mut part1 = 1;
     for (time, dist) in times.iter().zip(dists.iter()) {
-        let mut hold_time = 1;
-        let mut release_time = time - 1;
-
-        let mut ways_to_beat = 0;
-        while release_time > 0 {
-            if hold_time * release_time > *dist {
-                ways_to_beat += 1;
-            }
-            hold_time += 1;
-            release_time -= 1;
-        }
-        part1 *= ways_to_beat;
+        part1 *= ways_to_beat(*time, *dist);
     }
 
     // Part 2
-
-    let input: String = input.chars().filter(|x| x.is_ascii_digit() || *x == '\n').collect();
+    let input: String = input
+        .chars()
+        .filter(|x| x.is_ascii_digit() || *x == '\n')
+        .collect();
     let (time, dist): (u64, u64) = Numbers::new(&input).collect_tuple().unwrap();
+    let part2 = ways_to_beat(time, dist);
 
+    (part1, part2)
+}
+
+fn ways_to_beat(time: u64, dist: u64) -> u64 {
     let mut hold_time = 1;
     let mut release_time = time - 1;
-
-    let mut part2 = 0;
+    let mut res = 0;
     while release_time > 0 {
         if hold_time * release_time > dist {
-            part2 += 1;
+            res += 1;
         }
         hold_time += 1;
         release_time -= 1;
     }
-
-    (part1, part2)
+    res
 }
